86f66d308779dbfeae79d9c9d3a4c63fc4342111
mama <email@email.com>
2017-06-15 10:52:59 -0400

    ajout cookbook webserver default recette

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -3,3 +3,32 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+service 'firewalld' do
+  action [:disable, :stop]
+end
+
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
+end
+
+template '/var/www/html/index.html' do # ~FC033
+  source 'index.html.erb'
+end
