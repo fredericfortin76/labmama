0f39ee5a0d50090a3da1827b262f83dfd9106035
frederic fortin <fredericfortin76@gmail.com>
2017-06-16 09:44:34 -0400

    ajout tomcat8

--- cookbooks/db/recipes/default.rb
+++ cookbooks/db/recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
+# Load MySQL passwords from the 'passwords' data bag.
+# install wget pour aller chercher fichier remote
 yum_package 'wget'
 yum_package 'initscripts'
 
@@ -18,8 +20,10 @@ remote_file '/tmp/mysql57-community-release-el7-11.noarch.rpm' do
 end
 
 #inatallation du repo mysql
-execute 'rpm -ivh /tmp/mysql57-community-release-el7-11.noarch.rpm'
-
+execute 'install_repo' do
+  command 'rpm -ivh /tmp/mysql57-community-release-el7-11.noarch.rpm'
+  not_if 'rpm -qa mysql57-community-release'
+end
 
 #install mysql
 mysql_service 'foo' do
@@ -28,3 +32,15 @@ mysql_service 'foo' do
   initial_root_password 'Qwerty69'
   action [:create, :start]
 end
+
+
+# Create a mysql database on a named mysql instance
+#mysql_database 'oracle_rools' do
+#  connection(
+#    :host     => '127.0.0.1',
+#    :username => 'root',
+#    :socket   => "/var/run/mysql-foo/mysqld.sock",
+#    :password => 'Qwerty69'
+#  )
+#  action :create
+#end
--- cookbooks/tomcat8/metadata.rb
+++ cookbooks/tomcat8/metadata.rb
@@ -1,9 +1,9 @@
-name 'db'
+name 'tomcat8'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures db'
-long_description 'Installs/Configures db'
+description 'Installs/Configures tomcat8'
+long_description 'Installs/Configures tomcat8'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
@@ -11,13 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/db/issues'
+# issues_url 'https://github.com/<insert_org_here>/tomcat8/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/db'
-depends 'mysql', '~> 8.4.0'
-#depends 'database', '= 6.1.1'
-# depends 'mysql2_chef_gem', '~> 2.0'
+# source_url 'https://github.com/<insert_org_here>/tomcat8'
